bb4cf3fbe2bc024c60f43851d6e0a03cb95a7145
Leos <nguyenlethienphuc2004@gmail.com>
2023-03-18 10:16:45 -0500

    add bin/bash and modify school.sh

---
+++ school.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+cat Property_Tax_Roll.csv | grep "MADISON SCHOOLS" | cut -d ',' -f 7 | { sum=0; count=0; while read n; do sum=$(($sum+$n)); count=$(($count+1)); done; echo average=$(echo "scale=6; $sum/$count" | bc); }
